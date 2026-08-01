a79207df02291219e8457c255dbb1d55b44e2cc7
Leonardo Chavez <132311112+leonardoc101@users.noreply.github.com>
2026-08-01 13:30:13 -0600

    Add: products-of-array-discluding-self - submission-13

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-13.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> out;
+        for (int i = 0; i < nums.size(); i++) {
+            int prod = 1;
+            for (int j = 0; j < nums.size(); j++) {
+                if (i != j) {
+                    prod *= nums[j];
+                }        
+            }
+            out.push_back(prod);
+        }
+        return out;
+    }
+};

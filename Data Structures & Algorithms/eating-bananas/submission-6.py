a4c2b8e25f18d37b729167dd082bd1962b5cded5
Leonardo Chavez <132311112+leonardoc101@users.noreply.github.com>
2026-08-07 21:37:33 -0600

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,28 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+        low = 0
+        high = max(piles)
+        mid = int((low + high) / 2)
+        prev = 1
+        while low <= high:
+            if mid != 0:
+                t = self.simulate(piles, mid)
+            else:
+                break
+            if t > h:
+                low = mid + 1
+                mid = int((low + high) / 2)
+            elif t <= h:
+                high = mid - 1
+                prev = mid
+                mid = int((low + high) / 2)
+        return prev
+                
+    def simulate(self, piles, rate):
+        time = 0 
+        for pile in piles:
+            time += math.ceil(pile / rate)
+        return time
+

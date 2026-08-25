a0bb6aedf6d6500e793b834bd0529f1fa3789114
Leonardo Chavez <132311112+leonardoc101@users.noreply.github.com>
2026-08-25 12:58:30 -0600

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        fresh = set()
+        visited = set()
+        agenda = deque()
+
+        def add_rot(r, c):
+            if (r, c) not in fresh:
+                return
+            agenda.append((r, c))
+            visited.add((r, c))
+            fresh.remove((r, c))
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh.add((r, c))
+                if grid[r][c] == 2:
+                    agenda.append((r, c))
+                    visited.add((r, c))
+        minutes = 0
+        if not fresh:
+            return minutes
+        while agenda:
+            for i in range(len(agenda)):
+                r, c = agenda.popleft()
+                add_rot(r + 1, c)
+                add_rot(r - 1, c)
+                add_rot(r, c + 1)
+                add_rot(r, c - 1)
+            minutes += 1
+            if not fresh:
+                return minutes
+        return -1

6df6bd0e618f3aee5f5206790442db5faa40fac4
Leonardo Chavez <132311112+leonardoc101@users.noreply.github.com>
2026-08-24 21:14:44 -0600

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        seen = set() # (r, c) tuples
+        count = 0
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        def add_island(r, c):
+            agenda = deque()
+            agenda.append((r, c))
+            
+            while agenda:
+                r, c = agenda.popleft()
+                for d in directions:
+                    new_cell = (r + d[0], c + d[1])
+                    if new_cell in seen:
+                        continue
+                    if (0 <= new_cell[0] < ROWS) and (0 <= new_cell[1] < COLS) and (grid[new_cell[0]][new_cell[1]] == "1"):    
+                        agenda.appendleft(new_cell)
+                        seen.add(new_cell)    
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r, c) not in seen:
+                    count += 1
+                    add_island(r, c)
+        return count

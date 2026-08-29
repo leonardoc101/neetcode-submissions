3ac6db864251a370ee185aa39c0f4a403e1acbd1
Leonardo Chavez <132311112+leonardoc101@users.noreply.github.com>
2026-08-28 20:50:14 -0600

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        wildcard = 0
+        stack = [] # idx
+        wildcards = [] # idx
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            elif s[i] == ")":
+                if stack:
+                    stack.pop()
+                elif wildcards:
+                    wildcards.pop()
+                else:
+                    return False
+            else:
+                wildcards.append(i)
+        if not stack:
+            return True
+        else:
+            while stack:
+                if not wildcards:
+                    return False
+                if stack[-1] < wildcards[-1]:
+                    stack.pop()
+                    wildcards.pop()
+                else:
+                    return False
+            return True
+                

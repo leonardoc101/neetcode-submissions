081c5ca65c517cc8901aa94a9598a27f7a59bf4a
Leonardo Chavez <132311112+leonardoc101@users.noreply.github.com>
2026-08-28 19:51:29 -0600

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        can_reach = [False, False, False]
+        for j in range(3):
+            for i in range(len(triplets)):
+                if ((triplets[i][j] == target[j]) 
+                and (triplets[i][(j + 1) % 3] <= target[(j + 1) % 3])
+                and (triplets[i][(j + 2) % 3] <= target[(j + 2) % 3])):
+                    can_reach[j] = True
+                    break
+        return can_reach[0] and can_reach[1] and can_reach[2]
